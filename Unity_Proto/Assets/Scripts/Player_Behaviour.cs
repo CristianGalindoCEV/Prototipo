680bd1283d566d87024341a5606f2426f1438c86
CristianGalindoCEV <Cristiangalindo_bcnvj1ma1920@cev.com>
2020-10-06 18:40:47 +0200

    Salta rota y demas

--- Unity_Proto/Assets/Scripts/Player_Behaviour.cs
+++ Unity_Proto/Assets/Scripts/Player_Behaviour.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class Player_Behaviour : MonoBehaviour
 {
 
-    [SerializeField] private float m_speed = 4;
-    [SerializeField] private Transform myTransform;
-    private float m_xAxis, m_zAxis;
-
+    public float moveSpeed = 5;
+    public float rotateSpeed = 180;
+    public float jumpSpeed = 20;
+    public float gravity = 9.8f;
+    CharacterController controller;
+    Vector3 currentMovement;
 
     void Start()
     {
-        myTransform = GetComponent<Transform>();
+
+        controller = GetComponent<CharacterController>();
     }
-   
+
     void Update()
     {
-        m_xAxis = Input.GetAxis("Horizontal");
-        m_zAxis = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(- m_xAxis, 0,- m_zAxis);
-        myTransform.Translate(movement * m_speed * Time.deltaTime);
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime, 0);
+
+        currentMovement = new Vector3(0, currentMovement.y, Input.GetAxis("Vertical") * moveSpeed);
+        currentMovement = transform.rotation * currentMovement;
+
+        if (!controller.isGrounded)
+            currentMovement -= new Vector3(0, gravity * Time.deltaTime, 0);
+        else
+            currentMovement.y = 0;
+
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+            currentMovement.y = jumpSpeed;
+
+        controller.Move(currentMovement * Time.deltaTime);
     }
 }
