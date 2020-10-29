3a2b2c2e53e21f0d8e0da15cb0b6b754a05677d4
Cristian Galindo <cristiangalindo_bcnvj1ma1920@cev.com>
2020-10-29 16:37:58 +0100

    Tipografia + ItemVida

---
+++ Unity_Proto/Assets/Scripts/Props/HealItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            // StartCoroutine(MeTocan());
+            Destroy(gameObject);
+        }
+    }
+
+   /* IEnumerator MeTocan()
+    {
+        yield return new WaitForSeconds(1.0f);
+        //Sonido
+        //Particulas
+    }*/
+}
