01e35894c4a81b0ceb87bcf3e0c82b58578103f9
Cristian Galindo <cristiangalindo_bcnvj1ma1920@cev.com>
2020-10-29 19:04:05 +0100

    Player input + arma + bullet

--- Unity_Proto/Assets/Scripts/HUD & Cam/InputManager.cs
+++ Unity_Proto/Assets/Scripts/HUD & Cam/InputManager.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    [SerializeField] private GameObject Bauculo;
+    [SerializeField] private GameObject Espada;
+    public bool BauculoItem = false;
+    public bool EspadaItem = true;
+    public bool Desbloqueado = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Bauculo.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (BauculoItem == true)
+        {
+            Espada.SetActive(false);
+            Bauculo.SetActive(true);
+            EspadaItem = (false);
+            if (Input.GetButtonDown("Fire1"))
+            {
+                //Bauculo.Fire();
+            }
+        }
+        if (EspadaItem == true)
+        {
+            Espada.SetActive(true);
+            Bauculo.SetActive(false);
+            BauculoItem = (false);
+            if (Input.GetButtonDown("Fire1"))
+            {
+                //Espada.Fire();
+            }
+        }
+        if (Input.GetKey("1"))
+        {
+            EspadaItem = true;
+        }
+        if (Input.GetKey("2"))
+        {
+            BauculoItem = true;
+        }
     }
+
+
 }
